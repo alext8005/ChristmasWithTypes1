a502586cdb782124c6d4be1e83c203edf21a2359
Alejandra Trejo <alext8005@yahoo.com>
2025-02-18 21:13:51 -0700

    edited to delete system string printint

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -14,14 +14,14 @@ namespace ChristmasWithTypes
             xmas.Santa = "Kris Kringle";
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents = new string[] {"book", "laptop", "chocolate"};
+            xmas.Presents = new[] {"book", "laptop", "chocolate"};
 
         //TODO Set the TreeHeight to 10
             xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
-            Console.WriteLine($"Here are the presents we would like: {xmas.Presents} \n");
+            Console.WriteLine("Here are the presents we would like:");
 
             foreach (var present in xmas.Presents)
             {
